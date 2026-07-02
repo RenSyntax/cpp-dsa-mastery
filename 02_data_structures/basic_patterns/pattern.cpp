f35cc5323fe7156fffa0b2af8e681c01cb69bf01
RenSyntax <ren.syntax@gmail.com>
2026-07-02 22:19:41 +0530

    Daily Practice: Patten Problems

--- 02_data_structures/basic_patterns/pattern.cpp
+++ 02_data_structures/basic_patterns/pattern.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+// LECTURE EXAMPLES:
+
 // Pattern 1: 
 void pattern1(int n) {
   /*
@@ -140,6 +143,59 @@ void pattern7(int n) {
     cout << endl;
     
   }
+}
+
+---
+
+// Daily Practice:
+
+void pattern1(int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
+
+void pattern2(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
+
+void pattern3(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << j;
+    }
+    cout << endl;
+  }
+}
+
+void pattern4(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << i;
+    }
+    cout << endl;
+  }
+}
+
+void pattern5(int n) {
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n-i+1; j++) {
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
+
+void pattern6(int n) {
+
 }
 
 // backend:
